66ca0517ea420d5dffc64955ed74058f9424b7a7
IoanaRaluca <33445568+IoanaRaluca@users.noreply.github.com>
2017-11-12 13:09:41 +0200

    Update index.js

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,9 +1,11 @@
+var staticCacheName = 'wittr-static-v2';
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
     // TODO: change the site's theme, eg swap the vars in public/scss/_theme.scss
     // Ensure at least $primary-color changes
     // TODO: change cache name to 'wittr-static-v2'
-    caches.open('wittr-static-v1').then(function(cache) {
+    caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
         '/',
         'js/main.js',
@@ -19,6 +21,17 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     // TODO: remove the old cache
+    caches.keys().then(function(cacheNames){
+    	return Promise.all(
+    		cacheNames.filter( function(cacheName){ //i am interested in cache 
+    			//that begin with "wittr-", but that are not the static cache
+    			//this is a list of cache that i don't need anymore.
+    			return cacheName.startsWith("wittr-") && cacheName != staticCacheName;
+    		}).map(function(cacheName){
+    			return caches.delete(cacheName);
+    		})
+    	);
+    })
   );
 });
 
