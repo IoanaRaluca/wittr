9d98fe5adfa83d5a011b82a17169f624866ff6f5
IoanaRaluca <33445568+IoanaRaluca@users.noreply.github.com>
2017-11-12 11:48:39 +0200

    Update index.js

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -16,4 +16,14 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   // TODO: respond with an entry from the cache if there is one.
   // If there isn't, fetch from the network.
+  event.respondWith(
+  	caches.match(event.request).then(function(response){
+  		if(response){
+  			return response; //i have a match in the cache and i will return it
+  		}
+
+  		return fetch(event.request); //else i will return a fetch to
+  		//the network 
+  		})
+  	);
 });
